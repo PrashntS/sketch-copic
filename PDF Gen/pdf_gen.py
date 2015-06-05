f1227624cef539f824360b3849400e36d1c29405
Prashant Sinha <prashnts@live.com>
2015-06-05 23:33:25 +0530

    svg sample

--- PDF Gen/pdf_gen.py
+++ PDF Gen/pdf_gen.py
@@ -4,33 +4,43 @@
 import json
 import os
 import svgwrite
-from svgwrite import px, cm, mm
+from svgwrite import px, px, mm
 
 COLOR_LIB = {}
 
 def draw():
     dwg = svgwrite.Drawing(filename = "out.svg")
+
     hlines = dwg.add(dwg.g(id='hlines', stroke='#b3e3f1'))
 
+    dwg.add(dwg.rect(insert = (0*px, 0*px), size = (50*px, 20*px), fill = '#b3e3f1'))
+
+    dwg.add(dwg.text("Reported Crimes in Sweden",
+                                       insert = (410, 50),
+                                       fill = "rgb(255,255,0)",
+                                       style = "font-size:49px; font-family:P22UndergroundProDemi"))
+
+    """
     for y in range(20):
-        hlines.add(dwg.line(start=(2*cm, (2+y)*cm), end=(18*cm, (2+y)*cm)))
+        hlines.add(dwg.line(start=(20*px, (20+y)*px), end=(18*px, (2+y)*px)))
 
     vlines = dwg.add(dwg.g(id='vline', stroke='blue'))
     for x in range(17):
-        vlines.add(dwg.line(start=((2+x)*cm, 2*cm), end=((2+x)*cm, 21*cm)))
-    shapes = dwg.add(dwg.g(id='shapes', fill='red'))
+        vlines.add(dwg.line(start=((2+x)*px, 2*px), end=((2+x)*px, 21*px)))
 
-    # set presentation attributes at object creation as SVG-Attributes
-    shapes.add(dwg.circle(center=(15*cm, 8*cm), r='2.5cm', stroke='blue',
+    #shapes = dwg.add(dwg.g(id='shapes', fill='red'))
+
+        # set presentation attributes at object creation as SVG-Attributes
+    shapes.add(dwg.circle(center=(15*px, 8*px), r='2.5px', stroke='blue',
                           stroke_width=3))
 
     # override the 'fill' attribute of the parent group 'shapes'
-    shapes.add(dwg.rect(insert=(5*cm, 5*cm), size=(45*mm, 45*mm),
+    shapes.add(dwg.rect(insert=(5*px, 5*px), size=(45*mm, 45*mm),
                         fill='blue', stroke='red', stroke_width=3))
 
     # or set presentation attributes by helper functions of the Presentation-Mixin
-    ellipse = shapes.add(dwg.ellipse(center=(10*cm, 15*cm), r=('5cm', '10mm')))
-    ellipse.fill('green', opacity=0.5).stroke('black', width=5).dasharray([20, 20])
+    ellipse = shapes.add(dwg.ellipse(center=(10*px, 15*px), r=('5px', '10mm')))
+    ellipse.fill('green', opacity=0.5).stroke('black', width=5).dasharray([20, 20])"""
     dwg.save()
     pass
 
